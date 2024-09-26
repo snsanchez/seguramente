e2f5fa5df82726cc6415cc3344e88aeec8bb8a0c
san2 <152931005+snsanchez@users.noreply.github.com>
2024-09-26 17:38:37 -0300

    alias

--- app/database.py
+++ app/database.py
@@ -6,9 +6,18 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
+DATABASE_URL = os.getenv("POSTGRES_URL")  # La URL original de Vercel
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+
+engine = create_engine(DATABASE_URL)
+
+"""
+
 DATABASE_URL = os.getenv("POSTGRES_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL no está configurada en el entorno.")
+"""
 
 
 database = Database(DATABASE_URL)
